1fd46d4f7804b394a65a746b552438a8f92fd404
weam_ibrahim <122307908+weamibrahim@users.noreply.github.com>
2023-03-06 03:03:42 +0200

    Add files via upload

---
+++ slideuo,down/task2.js
@@ -0,0 +1,16 @@
+$(".im1").on("click",function () {
+    
+    $(".hide1").slideToggle()
+})
+$(".im2").on("click",function () {
+    
+    $(".hide2").slideToggle()
+})
+
+$(".im3").on("click",function () {
+    
+    $(".hide3").slideToggle()
+})
+
+
+
